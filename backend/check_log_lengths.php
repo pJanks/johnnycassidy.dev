5f504854528d5345d37d6320e070bf2f1c407826
pjanks <johnny.k.cassidy@gmail.com>
2022-11-21 09:17:16 -0800

    Adjust logging logic

--- backend/check_log_lengths.php
+++ backend/check_log_lengths.php
@@ -9,11 +9,12 @@
 
   $date = new DateTime();
   $formattedDate = $date->format('m.d.y h:i:s A');
+  $path = '/var/www/johnnycassidy.dev/logs/';
+
   $messageToLog = "$formattedDate: cronjob ran to check log lengths or it was forced\n";
-  file_put_contents('/var/www/johnnycassidy.dev/logs/cronjob.log', $messageToLog, FILE_APPEND);
+  file_put_contents($path . 'cronjob.log', $messageToLog, FILE_APPEND);
 
   foreach ($log_prefixes as $prefix) {
-    $path = '/var/www/johnnycassidy.dev/logs/';
     $log_file_contents = file_get_contents($path . $prefix . '.log');
     $split_logs = explode("\n", $log_file_contents);
     $countWithoutTrailingNewline = count($split_logs) - 1;
@@ -21,15 +22,13 @@
     if ($countWithoutTrailingNewline >= 10000) {
       $most_recent_split_logs = array_slice($split_logs, -5000, 5000);
       $most_recent_logs = implode("\n", $most_recent_split_logs);
-      $messageToLog = "$formattedDate: $prefix log cut in half to save memory\n";
+      $messageToLog = "$formattedDate: $prefix.log cut in half to save memory\n";
       file_put_contents($path . 'log.log', $most_recent_logs . $messageToLog);
-    } else {
-      echo "
-        <span style='font-size: 32px;'>
-          $prefix.log entries: $countWithoutTrailingNewline
-          <br>
-          <br>
-        </span>
-      ";
     }
+
+    echo "
+      <span style='font-size: 32px;'>
+        $prefix.log entries: $countWithoutTrailingNewline<br><br>
+      </span>
+    ";
   }
