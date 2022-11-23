104dea2b7e38c188a20a203398b7a14601a47dd6
pjanks <johnny.k.cassidy@gmail.com>
2022-11-23 08:09:01 -0800

    Refactor

--- global.js
+++ global.js
@@ -1,5 +1,6 @@
 const possibleRoute = location.pathname.split('/')[1].toLowerCase();
 const route = possibleRoute ? possibleRoute : 'home';
+
 if (route !== '404' && route !== 'sandbox') {
   const currentPageLink = document.querySelector(`.${route}-nav-list-item-link`);
   currentPageLink.href = '#';
