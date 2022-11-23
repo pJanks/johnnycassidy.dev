96ded501b378497a9f5679bdd96f7258b5af8c83
pjanks <johnny.k.cassidy@gmail.com>
2022-11-23 07:59:30 -0800

    Refactor

--- migrations/create_scores_table.php
+++ migrations/create_scores_table.php
@@ -5,16 +5,14 @@
   $date = $date->format('m.d.y h:i:s A');
 
   try {
-    $sql = '
-      CREATE TABLE scores (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        score INT(20) UNSIGNED,
-        name VARCHAR(255) NOT NULL,
-        time VARCHAR(30) NOT NULL,
-        pills_eaten INT(20) UNSIGNED,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      )
-    ';
+    $sql = 'CREATE TABLE scores (
+      id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      score INT(20) UNSIGNED,
+      name VARCHAR(255) NOT NULL,
+      time VARCHAR(30) NOT NULL,
+      pills_eaten INT(20) UNSIGNED,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )';
   
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
@@ -27,5 +25,4 @@
   } catch(Exception $e) {
     $errorMessage = $date . ' ERROR scores TABLE WASN\'T CREATED. ' . $e->getMessage() . ' line: ' . $e->getLine() . "\n";
     file_put_contents('logs/error.log', $errorMessage, FILE_APPEND);
-    // echo json_encode(['message' => $errorMessage], JSON_PRETTY_PRINT);
   }
