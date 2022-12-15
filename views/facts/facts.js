ef33e191bc20325c43bba71d919f764b22695e91
pjanks <johnny.k.cassidy@gmail.com>
2022-12-15 10:21:20 -0800

    Refactor random facts logic

--- views/facts/facts.js
+++ views/facts/facts.js
@@ -1,16 +1,24 @@
-const pre = document.querySelector('pre');
-const factSpan = document.querySelector('.fact');
 const getRandomFactButton = document.querySelector('.get-random-fact-button');
 const getTodaysFactButton = document.querySelector('.get-todays-fact-button');
+const factSpan = document.querySelector('.fact');
 
-getRandomFactButton.addEventListener('click', () => getFacts());
 getTodaysFactButton.addEventListener('click', () => getFacts('today'));
+getRandomFactButton.addEventListener('click', () => getFacts());
 
 const getFacts = async (randomOrToday = 'random') => {
-  const url = `https://uselessfacts.jsph.pl/${randomOrToday}.json?language=en`;
-  const response = await fetch(url);
-  const fact = await response.json();
-  factSpan.innerHTML = `${fact.text}<span class="asterisk">*</span>`;
+  try {
+    const url = `https://uselessfacts.jsph.pl/${randomOrToday}.json?language=en`;
+    const response = await fetch(url);
+
+    if (response.status === 200) {
+      const fact = await response.json();
+      factSpan.innerHTML = `${fact.text.trim()}<span class="asterisk">*</span>`;
+    } else if (response.status === 429) {
+      alert('There is a limit of 30 requests per minute. Please wait 60 seconds and then try again.');
+    }
+  } catch(err) {
+    alert(`There was an error: ${err}`);
+  }
 }
 
 getFacts();
